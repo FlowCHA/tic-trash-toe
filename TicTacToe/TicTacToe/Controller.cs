1d430342ee7c3a373c83596598f5faf7bad0fc89
FlowCHA <55203025+FlowCHA@users.noreply.github.com>
2019-09-11 14:56:56 -0400

    Initial Post T-T-T v_1
    Initial post of tic-tac-toe game with simple GUI and messy code.

---
+++ TicTacToe/TicTacToe/Controller.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class Controller
+    {
+        int counter = 1;
+        bool[] buttonsClicked = { false, false, false, false, false, false, false, false, false };
+        int[] spotsMarked = { 3, 4, 5, 6, 7, 8, 9, 10, 11};
+        bool winCon = false;
+
+        public Controller()
+        {
+
+        }
+
+        public int getCounter()
+        {
+            return counter;
+        }
+        public void updateCounter()
+        {
+            counter++;
+        }
+        public bool getButtonClicked(int a)
+        {
+            return buttonsClicked[a];
+        }
+        public void setButtonClicked(int a, bool b)
+        {
+            buttonsClicked[a] = b;
+        }
+        public void setSpotMark(int spot, int mark)
+        {
+            spotsMarked[spot] = mark;
+        }
+        public bool checkWin()
+        { 
+            if (spotsMarked[0] == spotsMarked[1] && spotsMarked[0] == spotsMarked[2])
+                winCon = true;
+            else if (spotsMarked[0] == spotsMarked[4] && spotsMarked[0] == spotsMarked[8])
+                winCon = true;
+            else if (spotsMarked[0] == spotsMarked[3] && spotsMarked[0] == spotsMarked[6])
+                winCon = true;
+            else if (spotsMarked[1] == spotsMarked[4] && spotsMarked[1] == spotsMarked[7])
+                winCon = true;
+            else if (spotsMarked[2] == spotsMarked[4] && spotsMarked[2] == spotsMarked[6])
+                winCon = true;
+            else if (spotsMarked[2] == spotsMarked[5] && spotsMarked[2] == spotsMarked[8])
+                winCon = true;
+            else if (spotsMarked[3] == spotsMarked[4] && spotsMarked[3] == spotsMarked[5])
+                winCon = true;
+            else if (spotsMarked[6] == spotsMarked[7] && spotsMarked[6] == spotsMarked[8])
+                winCon = true;
+
+            return winCon;
+        }
+    }
+}
---
+++ TicTacToe/TicTacToe/Form1.cs
@@ -0,0 +1,395 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "Player X starts the game.";
+        }
+        Controller controls = new Controller();
+        
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (!controls.getButtonClicked(0))
+            {
+                label1.Text = button1.Name + " clicked.";
+                if (controls.getCounter() % 2 == 1)
+                {
+                    button1.Text = "X";
+                    controls.setSpotMark(0, 1);
+                }
+                else if (controls.getCounter() % 2 == 0)
+                {
+                    button1.Text = "O";
+                    controls.setSpotMark(0, 2);
+                }
+                controls.updateCounter();
+                controls.setButtonClicked(0, true);
+            }
+            else
+            {
+                label1.Text = "You can't click that button.";
+            }
+
+            if(controls.checkWin())
+            {
+                if (controls.getCounter() % 2 == 0)
+                {
+                    label1.Text = "Player X wins.";
+                    for (int i = 0; i < 9; i++)
+                        controls.setButtonClicked(i, true);
+                }
+                if (controls.getCounter() % 2 == 1)
+                {
+                    label1.Text = "Player O wins.";
+                    for (int i = 0; i < 9; i++)
+                        controls.setButtonClicked(i, true);
+                }
+
+            }
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (!controls.getButtonClicked(1))
+            {
+                label1.Text = button2.Name + " clicked.";
+                if (controls.getCounter() % 2 == 1)
+                {
+                    button2.Text = "X";
+                    controls.setSpotMark(1, 1);
+                }
+                else if (controls.getCounter() % 2 == 0)
+                {
+                    button2.Text = "O";
+                    controls.setSpotMark(1, 2);
+                }
+                controls.updateCounter();
+                controls.setButtonClicked(1, true);
+            }
+            else
+            {
+                label1.Text = "You can't click that button.";
+            }
+
+            if (controls.checkWin())
+            {
+                if (controls.getCounter() % 2 == 0)
+                {
+                    label1.Text = "Player X wins.";
+                    for (int i = 0; i < 9; i++)
+                        controls.setButtonClicked(i, true);
+                }
+                if (controls.getCounter() % 2 == 1)
+                {
+                    label1.Text = "Player O wins.";
+                    for (int i = 0; i < 9; i++)
+                        controls.setButtonClicked(i, true);
+                }
+
+            }
+
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            if (!controls.getButtonClicked(2))
+            {
+                label1.Text = button3.Name + " clicked.";
+                if (controls.getCounter() % 2 == 1)
+                {
+                    button3.Text = "X";
+                    controls.setSpotMark(2, 1);
+                }
+                else if (controls.getCounter() % 2 == 0)
+                {
+                    button3.Text = "O";
+                    controls.setSpotMark(2, 2);
+                }
+                controls.updateCounter();
+                controls.setButtonClicked(2, true);
+            }
+            else
+            {
+                label1.Text = "You can't click that button.";
+            }
+
+            if (controls.checkWin())
+            {
+                if (controls.getCounter() % 2 == 0)
+                {
+                    label1.Text = "Player X wins.";
+                    for (int i = 0; i < 9; i++)
+                        controls.setButtonClicked(i, true);
+                }
+                if (controls.getCounter() % 2 == 1)
+                {
+                    label1.Text = "Player O wins.";
+                    for (int i = 0; i < 9; i++)
+                        controls.setButtonClicked(i, true);
+                }
+
+            }
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            if (!controls.getButtonClicked(3))
+            {
+                label1.Text = button4.Name + " clicked.";
+                if (controls.getCounter() % 2 == 1)
+                {
+                    button4.Text = "X";
+                    controls.setSpotMark(3, 1);
+                }
+                else if (controls.getCounter() % 2 == 0)
+                {
+                    button4.Text = "O";
+                    controls.setSpotMark(3, 2);
+                }
+
+                controls.updateCounter();
+                controls.setButtonClicked(3, true);
+            }
+            else
+            {
+                label1.Text = "You can't click that button.";
+            }
+
+            if (controls.checkWin())
+            {
+                if (controls.getCounter() % 2 == 0)
+                {
+                    label1.Text = "Player X wins.";
+                    for (int i = 0; i < 9; i++)
+                        controls.setButtonClicked(i, true);
+                }
+                if (controls.getCounter() % 2 == 1)
+                {
+                    label1.Text = "Player O wins.";
+                    for (int i = 0; i < 9; i++)
+                        controls.setButtonClicked(i, true);
+                }
+
+            }
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            if (!controls.getButtonClicked(4))
+            {
+                label1.Text = button5.Name + " clicked.";
+                if (controls.getCounter() % 2 == 1)
+                {
+                    button5.Text = "X";
+                    controls.setSpotMark(4, 1);
+                }
+                else if (controls.getCounter() % 2 == 0)
+                {
+                    button5.Text = "O";
+                    controls.setSpotMark(4, 2);
+                }
+                controls.updateCounter();
+                controls.setButtonClicked(4, true);
+            }
+            else
+            {
+                label1.Text = "You can't click that button.";
+            }
+
+            if (controls.checkWin())
+            {
+                if (controls.getCounter() % 2 == 0)
+                {
+                    label1.Text = "Player X wins.";
+                    for (int i = 0; i < 9; i++)
+                        controls.setButtonClicked(i, true);
+                }
+                if (controls.getCounter() % 2 == 1)
+                {
+                    label1.Text = "Player O wins.";
+                    for (int i = 0; i < 9; i++)
+                        controls.setButtonClicked(i, true);
+                }
+
+            }
+        }
+
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            if(!controls.getButtonClicked(5))
+            {
+                label1.Text = button6.Name + " clicked.";
+                if (controls.getCounter() % 2 == 1)
+                {
+                    button6.Text = "X";
+                    controls.setSpotMark(5, 1);
+                }
+                else if (controls.getCounter() % 2 == 0)
+                {
+                    button6.Text = "O";
+                    controls.setSpotMark(5, 2);
+                }
+                controls.updateCounter();
+                controls.setButtonClicked(5, true);
+            }
+            else
+            {
+                label1.Text = "You can't click that button.";
+            }
+
+            if (controls.checkWin())
+            {
+                if (controls.getCounter() % 2 == 0)
+                {
+                    label1.Text = "Player X wins.";
+                    for (int i = 0; i < 9; i++)
+                        controls.setButtonClicked(i, true);
+                }
+                if (controls.getCounter() % 2 == 1)
+                {
+                    label1.Text = "Player O wins.";
+                    for (int i = 0; i < 9; i++)
+                        controls.setButtonClicked(i, true);
+                }
+
+            }
+        }
+
+        private void Button7_Click(object sender, EventArgs e)
+        {
+            if (!controls.getButtonClicked(6))
+            {
+                label1.Text = button7.Name + " clicked.";
+                if (controls.getCounter() % 2 == 1)
+                {
+                    button7.Text = "X";
+                    controls.setSpotMark(6, 1);
+                }
+                else if (controls.getCounter() % 2 == 0)
+                {
+                    button7.Text = "O";
+                    controls.setSpotMark(6, 2);
+                }
+                controls.updateCounter();
+                controls.setButtonClicked(6, true);
+            }
+            else
+            {
+                label1.Text = "You can't click that button.";
+            }
+
+            if (controls.checkWin())
+            {
+                if (controls.getCounter() % 2 == 0)
+                {
+                    label1.Text = "Player X wins.";
+                    for (int i = 0; i < 9; i++)
+                        controls.setButtonClicked(i, true);
+                }
+                if (controls.getCounter() % 2 == 1)
+                {
+                    label1.Text = "Player O wins.";
+                    for (int i = 0; i < 9; i++)
+                        controls.setButtonClicked(i, true);
+                }
+
+            }
+        }
+
+        private void Button8_Click(object sender, EventArgs e)
+        {
+            if (!controls.getButtonClicked(7))
+            {
+                label1.Text = button8.Name + " clicked.";
+                if (controls.getCounter() % 2 == 1)
+                {
+                    button8.Text = "X";
+                    controls.setSpotMark(7, 1);
+                }
+                else if (controls.getCounter() % 2 == 0)
+                {
+                    button8.Text = "O";
+                    controls.setSpotMark(7, 2);
+                }
+                controls.updateCounter();
+                controls.setButtonClicked(7, true);
+            }
+            else
+            {
+                label1.Text = "You can't click that button.";
+            }
+
+            if (controls.checkWin())
+            {
+                if (controls.getCounter() % 2 == 0)
+                {
+                    label1.Text = "Player X wins.";
+                    for (int i = 0; i < 9; i++)
+                        controls.setButtonClicked(i, true);
+                }
+                if (controls.getCounter() % 2 == 1)
+                {
+                    label1.Text = "Player O wins.";
+                    for (int i = 0; i < 9; i++)
+                        controls.setButtonClicked(i, true);
+                }
+
+            }
+        }
+
+        private void Button9_Click(object sender, EventArgs e)
+        {
+            if (!controls.getButtonClicked(8))
+            {
+                label1.Text = button9.Name + " clicked.";
+                if (controls.getCounter() % 2 == 1)
+                {
+                    button9.Text = "X";
+                    controls.setSpotMark(8, 1);
+                }
+                else if (controls.getCounter() % 2 == 0)
+                {
+                    button9.Text = "O";
+                    controls.setSpotMark(8, 2);
+                }
+                controls.updateCounter();
+                controls.setButtonClicked(8, true);
+            }
+            else
+            {
+                label1.Text = "You can't click that button.";
+            }
+
+            if (controls.checkWin())
+            {
+                if (controls.getCounter() % 2 == 0)
+                {
+                    label1.Text = "Player X wins.";
+                    for (int i = 0; i < 9; i++)
+                        controls.setButtonClicked(i, true);
+                }
+                if (controls.getCounter() % 2 == 1)
+                {
+                    label1.Text = "Player O wins.";
+                    for (int i = 0; i < 9; i++)
+                        controls.setButtonClicked(i, true);
+                }
+
+            }
+        }
+    }
+}
